138ab60720293b8fa6c83d4fa4a27897f1ac8dd8
vitoriaa-lima <vitoria.psilva@sptech.school>
2025-11-15 20:12:49 -0300

    Atualizando barra de navegação lateral

--- web-data-viz/public/js/sessao.js
+++ web-data-viz/public/js/sessao.js
@@ -2,12 +2,13 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
+    
     var b_usuario = document.getElementById("b_usuario");
-
+    
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
         foto_usuario.innerHTML = nome[0].toUpperCase();
+        coisa_usuario.innerHTML = sessionStorage.COISA_USUARIO;
     } else {
         window.location = "../login.html";
     }
